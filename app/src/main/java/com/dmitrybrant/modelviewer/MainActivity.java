2e231452d0fdbc5bd1a5eec8a4d4dc52cabc1a4c
Dmitry Brant <dbrant@wikimedia.org>
2017-05-31 22:35:43 -0400

    Add About dialog.

--- app/src/main/java/com/dmitrybrant/modelviewer/MainActivity.java
+++ app/src/main/java/com/dmitrybrant/modelviewer/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContentResolverCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -118,6 +119,9 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.menu_open_model:
                 checkReadPermissionThenOpen();
+                return true;
+            case R.id.menu_about:
+                showAboutDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -264,5 +268,13 @@ public class MainActivity extends AppCompatActivity {
         } else {
             startActivity(new Intent(this, ModelGvrActivity.class));
         }
+    }
+
+    private void showAboutDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.about_text)
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
     }
 }
